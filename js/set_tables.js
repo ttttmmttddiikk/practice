1277f5e63e06009c603e69a5b4aa440ccee2131b
TakeKiyo <d8sk4ueum@gmail.com>
2020-04-02 21:09:07 +0900

    firebaseと接続

--- js/set_tables.js
+++ js/set_tables.js
@@ -1,18 +1,19 @@
+// Required for side-effects
+require("firebase/firestore");
 document.addEventListener('DOMContentLoaded',function(){
-
+console.log("test");
 // Firestoreのオブジェクト取得
 var db = firebase.firestore();
-
+console.log("test2");
 // このあたりにこれから掲載するサンプルコードなどを記述。
 var weeks = db.collection('weeks');
-
 var real_table_id;
-
 //weeksコレクションの中のtimeが一番最近のドキュメントを取得①
 weeks.orderBy("time", "desc").limit(1)
   .get()
   .then((querySnapshot) => {
      querySnapshot.forEach( (doc) => {
+       console.log(doc.data());
         //そのドキュメントのtable_idコレクションを取得②
         doc.collection("table_id").get().then(function(querySnapshot) {
             querySnapshot.forEach(function(doc2) {
